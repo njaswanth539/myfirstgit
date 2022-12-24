903d8c275052a3e4f205f5072bd3afececdf2084
najaswnth539 <njaswanth539@gmail.com>
2022-12-24 04:56:22 -0800

    first commit

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def jaswanth(request):
+    return HttpResponse('jasu is a good boys')
+    
